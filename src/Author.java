f9584441d2ab11b080cee393abb24c186fbc2970
Azerty <nik.k.2018@mail.ru>
2025-07-26 18:21:23 +0300

    Библиотечный справочник с отдельными классами

---
+++ src/Author.java
@@ -0,0 +1,20 @@
+// Author.java
+public class Author {
+    private String firstName;
+    private String lastName;
+
+    // Конструктор
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Геттеры
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,31 @@
+// Book.java
+public class Book {
+    private String title;
+    private Author author;
+    private int publicationYear;
+
+    // Конструктор
+    public Book(String title, Author author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    // Геттеры
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    // Сеттер только для года публикации
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
